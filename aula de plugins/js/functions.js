b346b6f90faf1ebc48e7c866191cfcdf60bfaadd
joao pedro <joaopedrobarreto1000@gmail.com>
2022-04-20 22:23:09 -0300

    novas aulas

--- aula de plugins/js/functions.js
+++ aula de plugins/js/functions.js
@@ -12,9 +12,11 @@ $(function(){
     //plugin mask.
 
     $('input[name=telefone]').mask('(99)99999-9999');
-    $('input[name=data]').css('border','0')
+    $('input[name=telefone]').css('width','110px');
+    $('input[name=telefone]').css('border-radius','10px');
+    $('input[name=data]').css('width','75px')
     $('inout[name=data]').css('outline','0')
-    $('input[name=data]').mask('99/99/9999', {placeholder:"    __/__/____"});
+    $('input[name=data]').mask('99/99/9999');
 
 
     $('a').fancybox();
