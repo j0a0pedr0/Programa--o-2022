34f001505ec4a6c323bf803a394e4ac80b73b251
joao pedro <joaopedrobarreto1000@gmail.com>
2022-05-10 02:19:20 -0300

    projetos novos

---
+++ aula-JS_ES6/pessoa1.js
@@ -0,0 +1,12 @@
+class Pessoa{
+    constructor(nome,idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+    getNome(){
+        return this.nome;
+    }
+    getIdade(){
+        return this.idade
+    }
+}
